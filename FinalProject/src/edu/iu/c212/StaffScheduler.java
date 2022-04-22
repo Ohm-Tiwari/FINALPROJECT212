182948b15153998a4026c4b31a49cf6cfb379b21
Olig, Henry David <olighenry@gmail.com>
2022-04-22 19:17:57 -0400

    Created method to get an array of staff through file

--- FinalProject/src/edu/iu/c212/StaffScheduler.java
+++ FinalProject/src/edu/iu/c212/StaffScheduler.java
@@ -1,10 +1,60 @@
 package edu.iu.c212;
 
+import edu.iu.c212.models.Staff;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class StaffScheduler
 {
+
+
+    static String fName;
+    static String lName;
+    static int age;
+    static String role;
+    static String availability;
+
+    public static void main(String[] args) throws FileNotFoundException {
+        createStaffList();
+    }
     // Methods
     public void ScheduleStaff()
     {
 
+
     }
+    public static ArrayList<Staff> createStaffList() throws FileNotFoundException {
+
+        ArrayList<Staff> inList = new ArrayList<>();
+        File inputFile = new File("src/edu/iu/c212/resources/staff_availability_IN.txt");
+        Scanner in = new Scanner(inputFile);
+
+
+        int index = 0;
+        while(in.hasNext()){
+            if (index == 0) {
+                fName = in.next();
+                index++;
+            } else if (index == 1) {
+                lName = in.next();
+                index++;
+            } else if (index == 2) {
+                age = Integer.parseInt(in.next());
+                index++;
+            } else if (index == 3) {
+                role = in.next();
+                index++;
+            } else if (index == 4) {
+                availability = in.next();
+                inList.add(new Staff((fName + " " + lName), age, role, availability));
+                index = 0;
+            }
+        }
+        return inList;
+    }
+
+
 }
--- FinalProject/src/edu/iu/c212/models/Staff.java
+++ FinalProject/src/edu/iu/c212/models/Staff.java
@@ -6,13 +6,15 @@ public class Staff
     private String fullName;
     private int age;
     private String role;
+    private String availability;
 
     // Constructor
-    public Staff(String fullName, int age, String role)
+    public Staff(String fullName, int age, String role, String availability)
     {
         this.fullName = fullName;
         this.age = age;
         this.role = role;
+        this.availability = availability;
 
     }
 
@@ -31,6 +33,8 @@ public class Staff
     {
         return role;
     }
+
+    public String getAvailability() { return availability; }
 
     public void setRole(String role){
         this.role = role;
