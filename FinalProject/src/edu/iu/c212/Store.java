b4df53d2480fdd925f003b33498113b3fa7b33ef
Olig, Henry David <olighenry@gmail.com>
2022-04-23 15:58:55 -0400

    Created method to get arraylist of items

--- FinalProject/src/edu/iu/c212/Store.java
+++ FinalProject/src/edu/iu/c212/Store.java
@@ -3,10 +3,20 @@ package edu.iu.c212;
 import edu.iu.c212.models.Item;
 import edu.iu.c212.models.Staff;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Store implements IStore
 {
+    static String name;
+    static String price;
+    static String quantity;
+    static String aisleNum;
+
+
     // Constructor
     public Store()
     {
@@ -21,6 +31,36 @@ public class Store implements IStore
     public void takeAction()
     {
 
+    }
+
+
+
+    public static ArrayList<Item> getItemList() throws FileNotFoundException {
+        ArrayList<Item> inList = new ArrayList<>();
+        File inputFile = new File("src/edu/iu/c212/resources/inventory.txt");
+        Scanner in = new Scanner(inputFile);
+        in.nextLine();
+        in.useDelimiter(",|\\n");
+        int index = 0;
+
+        while(in.hasNext()){
+            if (index == 0) {
+                name = in.next();
+                index++;
+            } else if (index == 1) {
+                price = in.next();
+                index++;
+            } else if (index == 2) {
+                quantity = in.next();
+                index++;
+            } else if (index == 3) {
+                aisleNum = in.next();
+                inList.add(new Item(name, price, quantity, aisleNum));
+                index = 0;
+            }
+        }
+
+        return inList;
     }
 
     @Override
--- FinalProject/src/edu/iu/c212/models/Item.java
+++ FinalProject/src/edu/iu/c212/models/Item.java
@@ -8,6 +8,11 @@ public class Item
     private int quantity;
     private int aisleNum;
 
+    private String tName;
+    private String tPrice;
+    private String tQuantity;
+    private String tAisleNum;
+
     // Constructor
     public Item(String name, double price, int quantity, int aisleNum)
     {
@@ -16,12 +21,24 @@ public class Item
         this.quantity = quantity;
         this.aisleNum = aisleNum;
     }
+    public Item(String name, String price, String quantity, String aisleNum)
+    {
+        this.tName = name;
+        this.tPrice = price;
+        this.tQuantity = quantity;
+        this.tAisleNum = aisleNum;
+    }
 
     // Methods
     public String getName()
     {
         return name;
     }
+
+    public String gettPRice() { return tPrice; }
+    public String gettQuantity() { return tQuantity; }
+    public String gettAisleNum() { return tAisleNum; }
+    public String gettName() { return tName; }
 
     public double getPrice()
     {
