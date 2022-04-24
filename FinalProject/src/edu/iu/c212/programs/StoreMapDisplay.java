400d859430e823dabf9613cbf4bc59d4a4824917
Josh Tonner <jtonner@iu.edu>
2022-04-24 09:19:03 -0400

    Store Map Stuff for now - Josh

--- FinalProject/src/edu/iu/c212/programs/StoreMapDisplay.java
+++ FinalProject/src/edu/iu/c212/programs/StoreMapDisplay.java
@@ -3,6 +3,7 @@ package edu.iu.c212.programs;
 import edu.iu.c212.models.Item;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class StoreMapDisplay
 {
@@ -10,11 +11,24 @@ public class StoreMapDisplay
     protected static int WIDTH;
     protected static int HEIGHT;
 
+    //If they want the store map to be fully static, may need to add some sort of arraylist to keep track of them during runtime, and iteratively set and display them - Josh
+
     // Methods
     public static void display(Item product)
     {
         JFrame mainFrame = new JFrame("High's Hardware Product Lookup: " + product);
 
-        // Note: Set WIDTH and LENGTH to 500 and 700 respectively, use it then to set frame
+        mainFrame.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+
+        // Add content pane
+        StoreMap storeMapDisplayComponent = new StoreMap(product.getAisle());
+        mainFrame.setContentPane(storeMapDisplayComponent);
+
+        // Set it's size
+        storeMapDisplayComponent.setPreferredSize(new Dimension(WIDTH/2, HEIGHT/2));
+
+        // Compile and show
+        mainFrame.pack();
+        mainFrame.setVisible(true);
     }
 }
