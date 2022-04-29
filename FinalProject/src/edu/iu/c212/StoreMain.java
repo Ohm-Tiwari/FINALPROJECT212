9f26229379fe4b25ce6c3b127abc2572c6acd1c7
Olig, Henry David <olighenry@gmail.com>
2022-04-29 19:00:46 -0400

    Update StoreMain.java

--- FinalProject/src/edu/iu/c212/StoreMain.java
+++ FinalProject/src/edu/iu/c212/StoreMain.java
@@ -3,20 +3,44 @@ package edu.iu.c212;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class StoreMain
-{
-    public static void main(String[] args) throws FileNotFoundException
-    {
+import static edu.iu.c212.Store.inputFile;
+
+public class StoreMain {
+    public static void main(String[] args) throws FileNotFoundException {
         Store store = new Store();
 
         //Instantiate and manage store object here
     }
+
     public static void readFileFunctions() throws FileNotFoundException {
         Scanner in = new Scanner(inputFile);
-        while(in.hasNextLine());
-        if(in.next().equals("ADD")){
+        while (in.hasNextLine()) ;
+        if (in.next().equals("ADD")) {
+
+        } else if (in.next().equals("COST")) {
+        } else if (in.next().equals("EXIT")) {
+
+        } else if (in.next().equals("FIND")) {
+
+        } else if (in.next().equals("FIRE")) {
+
+        } else if (in.next().equals("HIRE")) {
 
         }
-        else if(in.)
+        else if(in.next().equals("PROMOTE")){
+
+        }
+        else if(in.next().equals("SAW")){
+
+        }
+        else if(in.next().equals("SCHEDULE")){
+
+        }
+        else if(in.next().equals("SELL")){
+
+        }
+        else if(in.next().equals("QUANTITY")){
+            
+        }
     }
 }
