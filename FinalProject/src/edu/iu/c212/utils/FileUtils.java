638e46cf5470b43ad03f062269ea3c248a4fcbf7
Josh Tonner <jtonner@iu.edu>
2022-04-29 23:27:04 -0400

    Push

--- FinalProject/src/edu/iu/c212/utils/FileUtils.java
+++ FinalProject/src/edu/iu/c212/utils/FileUtils.java
@@ -34,7 +34,6 @@ public class FileUtils {
     // Should be adapted properly
     public static List<Item> readInventoryFromFile() throws IOException
     {
-        //stem.out.println("Tri");
         System.out.println(inventoryFile/*.toURI()*/.getPath() + "\n" + inventoryFile.exists());
         // depending on your OS, toURI() may need to be used when working with paths
 
