f2a2f3a638272d2a6229f2e35584d1bffe949666
Josh Tonner <jtonner@iu.edu>
2022-04-29 21:53:47 -0400

    Storemain updated

--- FinalProject/src/edu/iu/c212/StoreMain.java
+++ FinalProject/src/edu/iu/c212/StoreMain.java
@@ -1,13 +1,14 @@
 package edu.iu.c212;
 
+import edu.iu.c212.utils.FileUtils;
+
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 
 public class StoreMain {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args)
+    {
         Store store = new Store();
-
-        //Instantiate and manage store object here
     }
 }
